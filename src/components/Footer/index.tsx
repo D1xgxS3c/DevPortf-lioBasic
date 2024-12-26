5fab68dd43b73d8f34c6b8e4db07418733d0e947
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-12-26 18:43:22 -0300

    Criado Devportifólio em typeScript

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,69 @@
+import { Container, Flex, Box } from "@/styles/Global";
+import { Text } from "@/styles/Text";
+import { Footer as FooterWrapper } from "./style";
+import { UserImage } from "@/pages/home/style";
+import { userData } from "@/utils/userData";
+import { FaLinkedinIn, FaInstagram, FaFacebookF } from "react-icons/fa";
+import { Button } from "@/styles/Buttons";
+// import { HandEffect } from "../HandEffect";
+
+export const Footer = (): JSX.Element => {
+  return (
+    <FooterWrapper id="social-media">
+      <Container>
+        <Flex>
+          <UserImage
+            src={`src/public/static/img/logo/user.png`}
+            alt={userData.nameUser}
+            title={userData.nameUser}
+            width={"70px"}
+            height={"70px"}
+          />
+          <Box css={{ marginLeft: "$2" }}>
+            <Text type="heading4" color="grey5" css={{ marginBottom: "$2" }}>
+              Thank you! {/* <HandEffect /> */}
+            </Text>
+            <Text type="body1" color="grey2">
+              Me siga nas minhas redes sociais e vamos conversar...
+            </Text>
+          </Box>
+        </Flex>
+        <Flex
+          css={{
+            flexWrap: "wrap",
+            justifyContent: "center",
+            gap: "$2",
+          }}
+        >
+          <Button
+            className="instagram"
+            type="circle"
+            as="a"
+            target="_blank"
+            href={`https://www.instagram.com/D1XGXS3C/#`}
+          >
+            <FaInstagram />
+          </Button>
+          <Button
+            className="facebook"
+            type="circle"
+            as="a"
+            target="_blank"
+            href={`https://fb.com/${userData.facebookUser}`}
+          >
+            <FaFacebookF />
+          </Button>
+          <Button
+            className="linkedin"
+            type="circle"
+            as="a"
+            target="_blank"
+            href={`https://www.linkedin.com/in/diegocarvalhodev/`}
+          >
+            <FaLinkedinIn />
+          </Button>
+        </Flex>
+      </Container>
+    </FooterWrapper>
+  );
+};
---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Diego Carvalho",
+  githubUser: "D1xgxS3c",
+  linkedinUser: "diegocarvalhodev",
+  whatsappNumber: "+5511934293237",
+  emailUser: "87.c.die@@gmail.com",
+  instagramUser: "d1xgxS3c",
+  facebookUser: "",
+};
